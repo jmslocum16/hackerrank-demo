67f8e826996aca41b4c1b42fcec1f6d4af21b94e
Josh Slocum <jslocum@joshs-air.lan>
2022-05-15 12:02:47 -0500

    basic webapp + db counter example done

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,23 +1,19 @@
 from flask.cli import FlaskGroup
 
-from project import app, db, User
+from project import app
 
 
 cli = FlaskGroup(app)
 
-
+"""
 @cli.command("create_db")
 def create_db():
+    print("creating db")
     db.drop_all()
     db.create_all()
+    
     db.session.commit()
-
-
-@cli.command("seed_db")
-def seed_db():
-    db.session.add(User(email="michael@mherman.org"))
-    db.session.commit()
-
+"""
 
 if __name__ == "__main__":
     cli()
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,57 +1,56 @@
 import os
-
-from werkzeug.utils import secure_filename
+import psycopg2
 from flask import (
     Flask,
     jsonify,
     send_from_directory,
     request,
     redirect,
-    url_for
+    url_for,
+    render_template
 )
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
-db = SQLAlchemy(app)
+
+conn = psycopg2.connect("dbname=counters host=db port=5432  user=test_app password=test_app")
+# testing
+cur = conn.cursor()
+cur.execute('SELECT version()')
+
+# display the PostgreSQL database server version
+db_version = cur.fetchone()
+print(db_version)
+
+cur.close()
 
 
-class User(db.Model):
-    __tablename__ = "users"
+def get_counter():
+    cur = conn.cursor()
+    cur.execute('SELECT countvalue FROM counters WHERE ID=1')
+    cntval = cur.fetchone()[0]
+    print(cntval)
+    cur.close()
+    return cntval
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True, nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, email):
-        self.email = email
-
+def inc_counter():
+    cur = conn.cursor()
+    cur.execute('UPDATE counters SET countvalue = countvalue + 1 WHERE ID=1')
+    conn.commit()
+    cur.close()
 
 @app.route("/")
-def hello_world():
-    return jsonify(hello="world")
-
+def home():
+    inc_counter()
+    cntval = get_counter()
+    return render_template(
+        'home.html',
+        title="Demo Site",
+        cntval=cntval
+    )
 
 @app.route("/static/<path:filename>")
 def staticfiles(filename):
     return send_from_directory(app.config["STATIC_FOLDER"], filename)
 
 
-@app.route("/media/<path:filename>")
-def mediafiles(filename):
-    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
-
-
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return """
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
