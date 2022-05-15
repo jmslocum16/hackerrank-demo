c0ab02f7898666ac65e3607c6f1b32130689b6b2
Josh Slocum <jslocum@joshs-air.lan>
2022-05-15 16:19:03 -0500

    all load balancing algos implemented but async stuff is wrong

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -24,24 +24,56 @@ app.config.from_object("project.config.Config")
 
 count_by_url = defaultdict(int)
 latency_by_url = defaultdict(list)
+outstanding_by_url = defaultdict(int)
 backend_urls=['http://web_backend:4000', 'http://web_backend2:4001', 'http://web_backend3:4002']
 
-#conn = aiohttp.TCPConnector(limit=None, ttl_dns_cache=300)
-#session = aiohttp.ClientSession(connector=conn)
 
 # async stuff
-async def get_url(session, url, resource):
+async def get_url(session, url, resource, i):
+    print(str(time.time()) + ") " + str(i) + ":  start")
     start_time = time.perf_counter()
+    count_by_url[url] += 1
+    outstanding_by_url[url] += 1
     async with session.get(url + resource, ssl=False) as response:
+        print(str(time.time()) + ") " + str(i) + ":  sent")
         obj = await response.json()
+        print(str(time.time()) + ") " + str(i) + ":  done")
         print(obj)
         t = time.perf_counter() - start_time
         latency_by_url[url].append(t)
+        outstanding_by_url[url] -= 1
         return obj
 
+
+for u in backend_urls:
+    outstanding_by_url[u] = 0
+
+def get_shortest_queue():
+    min_v = 100000000
+    min_results = []
+    for k, v in outstanding_by_url.items():
+        if v <= min_v:
+            if v < min_v:
+                min_results = []
+                min_v = v
+            min_results.append(k)
+    print('min oustanding: ' + str(min_v))
+    return random.choice(min_results)
+
+next_url = 0
 def choose_url():
+    global next_url
     # TODO load balance?
-    return random.choice(backend_urls)
+    # random
+    # return random.choice(backend_urls)
+
+    # round robin
+    #url = backend_urls[next_url]
+    #next_url = (next_url + 1) %  len(backend_urls)
+    #return url
+
+    # shortest queue
+    return get_shortest_queue()
 
 def make_url(resource):
     backend_url = choose_url()
@@ -61,6 +93,7 @@ def inc_counter():
     print(result.text)
 """
 
+"""
 async def do_bench():
     async with aiohttp.ClientSession() as session:   
         tasks = [get_url(session, choose_url(), '/inc') for i in range(100)]
@@ -68,6 +101,27 @@ async def do_bench():
         cntval = await get_url(session, choose_url(), '/get')
         print(cntval['counter_value'])
         return cntval['counter_value']
+"""
+async def do_bench(rps, seconds):
+    write_pct = 0.2
+    write_tasks = int(rps * seconds * write_pct)
+    read_tasks = rps * seconds - write_tasks
+    task_resources = ['/inc' for i in range(write_tasks)] + ['/get' for j in range (read_tasks)]
+    random.shuffle(task_resources)
+
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for i in range(rps * seconds):
+            url = choose_url()
+            print(str(time.time()) + ") " + str(i) + ":  queue")
+            tasks.append(get_url(session, url, task_resources[i], i))
+            # TODO poisson
+            await asyncio.sleep(1.0 / rps)
+        await asyncio.gather(*tasks)
+        cntval = await get_url(session, choose_url(), '/get', 100000)
+        print(cntval['counter_value'])
+        return cntval['counter_value']
+
     
 def get_latency_percentiles(latencies, percentiles):
     sl = sorted(latencies)
@@ -77,16 +131,19 @@ def get_latency_percentiles(latencies, percentiles):
 def home():
     # inc_counter()
     # cntval = get_counter()
-    cntval = asyncio.run(do_bench())
+    cntval = asyncio.run(do_bench(10, 4))
     # print(str(latency_by_url))
     pctiles = [0.5, 0.9, 0.99]
     all_latencies = []
+    all_cnt = 0
     for x, y in latency_by_url.items():
         server_pctiles = get_latency_percentiles(y, pctiles)
-        print(x + ': (' + ' '.join([str(x) for x in pctiles]) + '): ( ' + ' '.join(server_pctiles) + ')')
+        server_cnt = count_by_url[x]
+        all_cnt += server_cnt
+        print(x + ' = ' + str(server_cnt) + ': (' + ' '.join([str(x) for x in pctiles]) + '): ( ' + ' '.join(server_pctiles) + ')')
         all_latencies += y
     all_pctiles = get_latency_percentiles(all_latencies, pctiles)
-    print('overall: (' + ' '.join([str(x) for x in pctiles]) + '): ( ' + ' '.join(all_pctiles) + ')')
+    print('overall: = ' + str(all_cnt) + ' (' + ' '.join([str(x) for x in pctiles]) + '): ( ' + ' '.join(all_pctiles) + ')')
     
     return render_template(
         'home.html',
